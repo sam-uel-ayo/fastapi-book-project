dbd8065514cd8c56be84cc25fa5db45816a55319
sam-uel-ayo <samuelayomide0705@gmail.com>
2025-02-10 21:42:34 +0100

    Added missing endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -5,6 +5,8 @@ from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
 
+from fastapi import APIRouter, status, HTTPException #
+
 router = APIRouter()
 
 db = InMemoryDB()
@@ -60,3 +62,17 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+# Add the missing endpoint
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    book = db.get_book(book_id)
+    if not book:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Book not found",
+        )
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=book.model_dump(),
+    )
